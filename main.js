51226cda36575942a138f331866c715893dac36f
tikogakhutishvili <tikogakhutishvili26@gmail.com>
2023-06-21 23:57:37 +0400

    array task

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-arr = [1,9,-3,11,-40,-57,34,100,-23]
-pnums = 0
+let arr = [1,9,-3,11,-40,-57,34,100,-23]
+let pnums = []
 for(let i=0; i<arr.length; i++){
     if(arr[i] > 0){
-        pnums = arr[i]
+        pnums.push(arr[i])
     }
-    console.log(pnums)
 }
+console.log(pnums)
+// task 2
 
-Arr = [-60,4,9,78,-17,33,2,-99,8,1,80]
-odd = []
+let Array = ["tiko", 1, -44, true, "nazibrola",56, "nazibrola", false, 88,-44 ]
+let nodouble = []
+for (let i = 0; i < Array.length; i++){
+    if(!nodouble.includes(Array[i])){
+     nodouble.push(Array[i])
+    } 
+}
+console.log(nodouble)
+
+
+// task 3
+let Arr = [-60,4,9,78,-17,33,2,-99,8,1,80]
+let odd = []
 even = []
 for(let i = 0; i < Arr.length; i++){
     if(Arr[i]%2 === 0 && Arr[i] !== 0){
